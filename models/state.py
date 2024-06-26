7ddc713ea2f1f3673278e24e27bd60f36f942744
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-06-26 21:17:41 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    
+    __tablename__ = 'states'
