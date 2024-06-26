2f5f9730260b28186114b33ae7301b6cb4669051
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-06-26 21:06:07 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    __tablename__ = 'tablename'
