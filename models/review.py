d98dcd3f11caf1815383684629e15a8b7948939c
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-06-27 05:59:49 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel):
