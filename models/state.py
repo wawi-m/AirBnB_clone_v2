42a3716f50e428e7c1545ca1c291af6ab84a60d9
wawi-m <122799427+wawi-m@users.noreply.github.com>
2024-09-03 02:10:27 +0300

    Create state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
 from models.base_model import BaseModel, Base
 from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import models
 
 
@@ -11,12 +12,16 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    cities = relationship("City",
+                          backref="state", cascade="all, delete,delete-orphan")
 
-    if models.storage_t == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
-    else:
+    if getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            """Returns the list of City instances with state_id equals to the current State.id"""
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+            """ returns list of City instances related to state """
+            from models import storage
+            list_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
